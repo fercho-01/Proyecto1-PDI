64a9b74e57cbb256b799f67b198abd64fcde01a5
fercho-01 <luisfernandorzcrzc@gmail.com>
2017-03-11 23:19:44 -0500

    Refactor

--- main.m
+++ main.m
@@ -1,71 +1,18 @@
-clear all,close all,clc;
-a = imread('imagen.jpg');
+%----------------------------------
+%   Tarea 1, Main
+%   Por:
+%       - Luis Fernando Orozco (lfernando.orozco@udea.edu.co)
+%         Estudiante de Ingeniería de Sistemas, Udea
+%         CC 1216716983
+%       - Santiago Sanmartin (santiago.sanmartin@udea.edu.co)
+%         Estudiante de Ingenieria de Sistemas, Udea
+%         CC 1017209945
+%       V1 Marzo 2017
 
-b1=edge(a,'sobel','vertical'); 
-b2=edge(a,'sobel','horizontal'); 
-b3=a;
-%b3=edge(a,'sobel'); 
+%Inicializo el sistema
+clear all;  %Inicializa todas las variables
+close all;  %Cierra todas las ventanas, archivos y procesos abiertos
+clc;        %Limpia la ventana de comandos
 
-%b3 = ~b3;
-%figure(1);
-%imshow(b3);
-
-
-tamanoInicial = 300;
-tamanoFinal = 60;
-%a = imread('unacosa.png');
-escala = tamanoFinal / tamanoInicial;
-b3 = imresize(b3,escala);
-figure(2); imshow(b3);
-
-%b3=edge(b3,'sobel'); 
-
-%b3 = ~b3;
-b3 = im2bw(a,0.5);
-b3 = imresize(b3,escala);
-figure(3);
-imshow(b3);
-txt = string('');
-state = 0; %cerp arriba 1 abajo
-temp = string('');
-threeCount = 0;
-for i=1:60
-   for j=1:60
-       if(b3(i,j)==1) 
-           if(state)
-              state = 0;
-              temp = string('2'); 
-              txt = strcat(txt,temp);
-           end
-           
-       else
-           if(~state)
-               state = 1;
-               temp = string('4');
-               txt = strcat(txt,temp);
-           end
-       end
-       temp = string('3');        
-       threeCount= threeCount+1;    
-       txt = strcat(txt,temp);    
-   end
-      if(state==1)
-          state = 0;
-          temp = string('2');
-          txt = strcat(txt,temp);
-      end
-   
-   temp = string('5');
-   txt = strcat(txt,temp);
-   temp = string('');
-   for i = 1:threeCount
-       temp = strcat('1',temp);
-   end
-   threeCount = 0;
-   txt = strcat(txt,temp);
-   
-end
-f = fullfile('c:\','myfiles','salida.txt')
-fi = fopen(f, 'w');
-fprintf(fi, txt);
-fclose(fi);
+%Lanzo la interfaz grafica
+myFigure();
--- myFigure.m
+++ myFigure.m
@@ -1,30 +1,16 @@
+%----------------------------------
+%   Tarea 1, Interfaz Grafica
+%   Por:
+%       - Luis Fernando Orozco (lfernando.orozco@udea.edu.co)
+%         Estudiante de Ingeniería de Sistemas, Udea
+%         CC 1216716983
+%       - Santiago Sanmartin (santiago.sanmartin@udea.edu.co)
+%         Estudiante de Ingenieria de Sistemas, Udea
+%         CC 1017209945
+%       V1 Marzo 2017
+
+
 function varargout = myFigure(varargin)
-% MYFIGURE MATLAB code for myFigure.fig
-%      MYFIGURE, by itself, creates a new MYFIGURE or raises the existing
-%      singleton*.
-%
-%      H = MYFIGURE returns the handle to a new MYFIGURE or the handle to
-%      the existing singleton*.
-%
-%      MYFIGURE('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in MYFIGURE.M with the given input arguments.
-%
-%      MYFIGURE('Property','Value',...) creates a new MYFIGURE or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before myFigure_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to myFigure_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help myFigure
-
-% Last Modified by GUIDE v2.5 27-Feb-2017 21:59:42
-
-% Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
@@ -41,61 +27,42 @@ if nargout
 else
     gui_mainfcn(gui_State, varargin{:});
 end
-% End initialization code - DO NOT EDIT
 
-
-% --- Executes just before myFigure is made visible.
 function myFigure_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to myFigure (see VARARGIN)
-
-% Choose default command line output for myFigure
 handles.output = hObject;
 
-% Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes myFigure wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
-
-
-% --- Outputs from this function are returned to the command line.
-function varargout = myFigure_OutputFcn(hObject, eventdata, handles) 
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Get default command line output from handles structure
 varargout{1} = handles.output;
 
-
-% --- Executes on button press in pushbutton1.
+%Evento ejecutado al oprimir el boton iniciar
 function pushbutton1_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global vid
-global data
-global sizeBox
-global offsetX offsetY
-global continuar
-continuar = true;
-vid = videoinput('winvideo',1,'MJPG_640x480');
-set(vid, 'FramesPerTrigger', Inf);
-set(vid, 'ReturnedColorspace', 'rgb')
-vid.FrameGrabInterval = 5;
-start(vid);
 
+%Declaración de variables globales
+global vid                  %elemento de video
+global data                 %Imagenes capturada por la camara web
+global sizeBox              %Tamaño del cuadrado rojo
+global offsetX offsetY      %Ubicación X y Y, del cuadrado rojo
+global continuar            %Variable de control para saber cuando dejar de capturar imagenes
+
+%Inicialización de variables
+continuar = true;       
 sizeBox = 300;
 offsetX = 480/2 - sizeBox/2;
 offsetY = 640/2 - sizeBox/2;
+vid = videoinput('winvideo',1,'MJPG_640x480'); %Se configura la entrada de video
+set(vid, 'FramesPerTrigger', Inf);
+set(vid, 'ReturnedColorspace', 'rgb')
+vid.FrameGrabInterval = 5;
+
+%Se inicia la entrada de video
+start(vid); 
+
+%Se procesan todas las imagenes proporcionadas por la camara
 while(continuar)
-   data = getsnapshot(vid);
+   data = getsnapshot(vid); %Captura de la imagen de la camara
    
+   %Pintar cuadrado rojo
    for i=1:sizeBox
        for j=1:sizeBox
            if(i==1 || j==1 || i==sizeBox || j==sizeBox)
@@ -105,38 +72,86 @@ while(continuar)
        end
        
    end
+   %Se muestra la imagen con el cuadrado rojo
    imshow(data);
 end
 
 
 
-
-% --- Executes on button press in pushbutton3.
+%Evento ejecutado al oprimir el boton "Tomar Foto"
 function pushbutton3_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton3 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global vid
-global data
-global sizeBox
-global offsetX offsetY
-global continuar
+
+%declaración de variables globales
+global vid              %Elemento de video
+global data             %Imagen capturada por la camara web
+global sizeBox          %Tamaño del cuadro rojo
+global offsetX offsetY  %Ubicación X y Y del cuadro rojo
+global continuar        %Variable de control usada para saber cuando dejar de capturar imagenes
+
+%Inicialización de las variables
 continuar = false;
+tamanoInicial = 300;                    %Tamaño de la porción de la imagen a procesar
+tamanoFinal = 60;                       %Tamaño de la imagen final
+escala = tamanoFinal / tamanoInicial;   %Escala de reducción de la imagen
+txt = string('');                       %cadena de texto con los comandos de la imagen para la impresora
+state = 0;                              %Variable de control para saber si el marcador pinta o no
+temp = string('');                      %variable temporal con un comando para la impresora
+threeCount = 0;                         %Conteo de pasos horizontales
 
-%data = rgb2gray(data);
+%Iniciar el procesamiento de la imagen
+data = im2bw(data,0.5); %convertir la imagen a blanco y negro
+stop(vid); %se para la captura de imagenes por la camara web
+img=data(offsetX+2:offsetX+sizeBox-1,offsetY+2:offsetY+sizeBox-1); %se extrae la porcion de la imgen que esta en el cuadrado rojo
+b3 = img; 
+b3 = imresize(b3,escala); %se cambia la resolución de la imagen
+imshow(b3); %se muestra la imagen
 
-data = im2bw(data,0.5);
+%Generar la cadena con los comandos
+%   1 -> Retrocede un paso
+%   2 -> Sube el marcador
+%   3 -> Realiza un paso
+%   4 -> Baja el marcador
+%   5 -> Realiza un salto de linea
 
-%imshow(data);
-stop(vid);
-img=data(offsetX+2:offsetX+sizeBox-1,offsetY+2:offsetY+sizeBox-1);
-imwrite(img,'imagen.jpg');
-img=edge(img,'sobel'); 
+for i=1:60
+   for j=1:60
+       if(b3(i,j)==1)                   %pixel blanco
+           if(state)                    %si el marcador esta abajo
+              state = 0;                %sube el marcador
+              temp = string('2');       %crea el comando para subir el marcador
+              txt = strcat(txt,temp);   %lo concatena en la cadena
+           end
+           
+       else                             %pixel negro
+           if(~state)                   %si el marcador esta arriba
+               state = 1;               %baja el marcador
+               temp = string('4');      %crea el comando para bajar el marcador
+               txt = strcat(txt,temp);  %lo concatena en la cadena
+           end
+       end
+       temp = string('3');              %avanza un paso      
+       threeCount= threeCount+1;        %se actualiza la cantidad de pasos realizados
+       txt = strcat(txt,temp);          %se añade el comando a la cadena de comandos   
+   end
+      if(state==1)                      %al finalizar una fila si el marcador termina abajo
+          state = 0;                    %sube el marcador
+          temp = string('2');           %crea el comando
+          txt = strcat(txt,temp);       %se añade el comando a la cadena de comandos
+      end
+   
+   temp = string('5');                  %se crea comando de linea nueva
+   txt = strcat(txt,temp);              %se añade el comando a la cadena de comandos
+   temp = string('');                   %se limpia la variable temporal
+   for i = 1:threeCount
+       temp = strcat('1',temp);         %se crea un comando de paso en sentido contrario por cada paso realizado
+   end
+   threeCount = 0;                      %se reinicia la cantidad de pasos realizados
+   txt = strcat(txt,temp);              %se añade la lista de comandos a la cadena de comandos
+   
+end
 
-img = ~img;
-imshow(img);
-
-%img=edge(img,'sobel'); 
-
-%img = ~img;
-imshow(img);
+%Creación del archivo con los comandos
+f = fullfile('c:\','myfiles','salida.txt')  %ruta donde se crea el archivo
+fi = fopen(f, 'w');                         %Se crea un archivo de escritura
+fprintf(fi, txt);                           %se imprime la cadena de comandos en el archivo
+fclose(fi);                                 %se cierra el archivo
